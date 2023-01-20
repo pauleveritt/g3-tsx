b7ce7174aa8c477c6cd425dd8d616b78f70589c2
pauleveritt <pauleveritt@me.com>
2023-01-20 14:10:01 -0500

    Write a TipLayout test that we can trust. Switch back to dumb-as-a-rock fixture data.

--- _includes/resources/tip/TipLayout.test.tsx
+++ _includes/resources/tip/TipLayout.test.tsx
@@ -2,19 +2,31 @@ import { expect, test } from "vitest";
 import { screen } from "@testing-library/dom";
 
 import { TipLayout } from "./TipLayout.11ty";
-import { EleventyPage } from "../../models";
+import { TipResource } from "./TipModels";
+import { AuthorReference } from "../../references/author/AuthorModels";
 
 test("TipLayout", () => {
-  const tipPage: EleventyPage = { fileSlug: "some-slug" };
-  // @ts-ignore
-  const tipData = { ...globalThis.tipData, page: tipPage };
-  tipData.collections.author = [
-    {
-      data: { title: "Some Author" },
-      page: { fileSlug: "some-slug" },
-    },
-  ];
-  document.body.innerHTML = TipLayout(tipData);
+  const tip: TipResource = {
+    title: "Some Title",
+    slug: "some-slug",
+    resourceType: "tip",
+  };
+  const content = "<p>some content</p>";
+  const author: AuthorReference = {
+    title: "Some Author",
+    slug: "some-slug",
+    resourceType: "author",
+    label: "sa",
+    resources: [],
+    referenceResources: [],
+  };
+
+  document.body.innerHTML = TipLayout(tip, content, author);
   const results = screen.getAllByRole("button");
   expect(results).to.exist;
+
+  const authorLink: HTMLAnchorElement = screen.getByRole("link", {
+    name: "Some Author",
+  });
+  expect(authorLink.href).to.equal("some-slug");
 });
--- sites/webstorm-guide/tips/add-script-tag-to-html/index.md
+++ sites/webstorm-guide/tips/add-script-tag-to-html/index.md
@@ -15,5 +15,5 @@ animatedGif:
   width: 600
   height: 300
 leadin: |
-  Drag the script file to your HTML code and drop it there to quickly add the `<script>` tag.
+  Drag the script file to your HTML code and drop it there to quickly add the `script` tag.
   
