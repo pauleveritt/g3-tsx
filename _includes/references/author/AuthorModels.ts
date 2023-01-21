58cad52185bf13ccd894884e46738391038cb6e8
pauleveritt <pauleveritt@me.com>
2023-01-21 10:49:35 -0500

    Export a collection type.

--- _includes/references/author/AuthorModels.ts
+++ _includes/references/author/AuthorModels.ts
@@ -5,6 +5,7 @@ import { EleventyPage } from "../../models";
 
 export const AuthorReference = Type.Intersect([Reference]);
 export type AuthorReference = Static<typeof AuthorReference>;
+export type AuthorCollection = { [name: string]: AuthorReference };
 
 export function getAuthor(data: any, page: EleventyPage): AuthorReference {
   const author: AuthorReference = {
